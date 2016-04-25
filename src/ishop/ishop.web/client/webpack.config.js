22204a09bc34f4711b4168c99cdfe3a2fdc6e076
andreicvasniuc <andrei.a.cvasniuc@gmail.com>
2016-04-25 22:58:13 +0300

    admin: configured image-loader

--- src/ishop/ishop.web/client/webpack.config.js
+++ src/ishop/ishop.web/client/webpack.config.js
@@ -68,7 +68,14 @@ module.exports = {
       { 
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
         loader: 'url?limit=10000&mimetype=image/svg+xml' 
-      }
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+            'file?hash=sha512&digest=hex&name=[hash].[ext]',
+            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
+    }
     ]
   }
 };
