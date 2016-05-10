08fe4377cfaf442f69a67b05b335e56feb3eb692
andreicvasniuc <andrei.a.cvasniuc@gmail.com>
2016-05-10 23:48:32 +0300

    admin: impleemnted showEmptyMessage directive

--- src/ishop/ishop.web/client/src/admin/directives/show-empty-message/index.jsx
+++ src/ishop/ishop.web/client/src/admin/directives/show-empty-message/index.jsx
@@ -1,58 +1,21 @@
 import angular from 'angular';
+import $ from 'jquery';
 // import loadingIcon from 'assets/images/loading.gif';
-
-class ShowEmtyMessageController {
-  constructor($scope, $element, $attrs, $compile, $timeout) {
-    //this.loadingIcon = loadingIcon;
-
-    var isEmpty = $attrs.isEmpty;
-    var msg = ($attrs.showEmptyMessage) ? $attrs.showEmptyMessage : 'Nothing to display';
-    var template = "<div class='ui-grid-row.even ui-grid-no-results' ng-show='" + isEmpty + "'>" + msg + "</div>";
-    var tmpl = angular.element(template);
-    $compile(tmpl)($scope);
-    $timeout(function () {
-        console.log('el',$element.find(".ui-grid-viewport"), tmpl);
-        $element.find(".ui-grid-viewport").append(tmpl);
-    }, 200);
-  }
-}
 
 let showEmptyMessage = {
   restrict: 'A',
-  //controller: ShowEmtyMessageController,
-  compile: function compile( tElement, tAttributes ) {
-            console.log(' (compile)', arguments  );
-            return function (scope, element, attrs) {
-                var isEmpty = attrs.isEmpty;
-                var msg = (attrs.showEmptyMessage) ? attrs.showEmptyMessage : 'Nothing to display';
-                var template = "<div class='ui-grid-row.even ui-grid-no-results' ng-show='" + isEmpty + "'>" + msg + "</div>";
-                var tmpl = angular.element(template);
-                //$compile(tmpl)($scope);
-                setTimeout(function () {
-                    console.log('el',element, tmpl);
-                    element.find(".ui-grid-viewport").append(tmpl);
+  compile: ( tElement, tAttributes ) => {
+            return (scope, element, attrs) => {
+                let isEmpty = attrs.isEmpty;
+                let msg = (attrs.showEmptyMessage) ? attrs.showEmptyMessage : 'Nothing to display';
+                let template = "<div class='ui-grid-row.even ui-grid-no-results' ng-show='" + isEmpty + "'>" + msg + "</div>";
+                let tmpl = angular.element(template);
+                setTimeout(() => {
+                    $(element).find(".ui-grid-viewport").append(tmpl);
                     scope.$apply();
-                }, 3000);
+                });
             };
          }
 };
 
 export default () => { return showEmptyMessage; };
-
-
-// complianceTrackerModule.directive('showEmptyMessage', function ($compile, $timeout) {
-
-//     return {
-//         restrict: 'A',
-//         link: function (scope, element, attrs) {
-//             var isEmpty = attrs.isEmpty;
-//             var msg = (attrs.showEmptyMessage) ? attrs.showEmptyMessage : 'Nothing to display';
-//             var template = "<div class='ui-grid-row.even ui-grid-no-results' ng-show='" + isEmpty + "'>" + msg + "</div>";
-//             var tmpl = angular.element(template);
-//             $compile(tmpl)(scope);
-//             $timeout(function () {
-//                 element.find(".ui-grid-viewport").append(tmpl);
-//             }, 200);
-//         }
-//     };
-// });
