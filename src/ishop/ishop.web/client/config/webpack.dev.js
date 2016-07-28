5b6f7007bae0a55a71933d584672f3dc84543ab1
andrew <andrew-PC@andrews-MacBook-Pro.local>
2016-07-28 18:26:06 +0300

    admin: added html-webpack-plugin and extract-text-webpack-plugin packages * used them to bundle scripts and styles in index.ejs (index.html)

---
+++ src/ishop/ishop.web/client/config/webpack.dev.js
@@ -0,0 +1,103 @@
+var _ = require( 'lodash' );
+var path = require( 'path' );
+//var webpack = require( 'webpack' );
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var pathTo = function() {
+    return path.join( __dirname, '../src', path.join.apply( path, arguments ) );
+}
+
+var pathApp = _.partial( pathTo, 'app' );
+var pathAdmin = _.partial( pathTo, 'admin' );
+
+module.exports = {
+  //context: pathApp(),
+  entry: {
+    admin: pathAdmin('index.jsx')//,
+    //app:  pathApp('index.jsx')
+  },
+
+  output: {
+    path: '../../public/javascripts',
+    //path: pathApp(),
+    filename: '[name].js', // Template based on keys in entry above
+    publicPath: '/'
+  },
+
+  resolve: {
+      // you can now require('file') instead of require('file.js')
+      extensions: [ '', '.js', '.jsx' ],
+      alias: {
+          //app sub aliases
+          //app: pathApp( 'index.js' ),
+          // controllers: pathApp( 'controllers' ),
+          // components: pathApp( 'components' ),
+          // services: pathApp( 'services' ),
+
+          //assets sub aliases
+          assets: pathTo( 'assets' )
+      }
+  },
+
+  devServer: {
+    headers: {"Access-Control-Allow-Origin": "http://localhost:3000"}
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx$/, 
+        loader: 'babel', 
+        query: { presets: ['es2015'] }, 
+        exclude: /node_modules/
+      },
+      {
+        test: /\.html$/, 
+        loader: 'ngtemplate?relativeTo=' + (path.resolve(__dirname, '../src')) + '/!html', 
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/, 
+        loader: ExtractTextPlugin.extract('style', 'css')
+      },
+      {
+        test: /\.styl$/, 
+        loader: 'style!css!stylus', 
+        exclude: /node_modules/
+      },
+      { 
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'file' 
+      },
+      { 
+        test: /\.(woff|woff2)($|\?)/, 
+        loader:'url?prefix=font/&limit=5000' 
+      },
+      { 
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url?limit=10000&mimetype=application/octet-stream' 
+      },
+      { 
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url?limit=10000&mimetype=image/svg+xml' 
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+            'file?hash=sha512&digest=hex&name=[hash].[ext]',
+            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/admin/index.ejs'
+    }),
+
+    new ExtractTextPlugin('[name].css')
+  ]
+};
