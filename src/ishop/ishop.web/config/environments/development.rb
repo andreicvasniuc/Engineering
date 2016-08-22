4a2c2549e3efcaa508fc55ead353854435626987
andrew <andrew-PC@andrews-MacBook-Pro.local>
2016-08-22 22:06:58 +0300

    added necesary code to implement CORS in development mode only

--- src/ishop/ishop.web/config/environments/development.rb
+++ src/ishop/ishop.web/config/environments/development.rb
@@ -29,6 +29,23 @@ Rails.application.configure do
       'Access-Control-Request-Method' => %w{GET POST PUT DELETE}.join(",")
   }
 
+  config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
+      allow do
+        origins '*'
+
+        resource '/cors',
+          :headers => :any,
+          :methods => [:post],
+          :credentials => true,
+          :max_age => 0
+
+        resource '*',
+          :headers => :any,
+          :methods => [:get, :post, :delete, :put, :patch, :options, :head],
+          :max_age => 0
+      end
+    end
+
   # Set the logging destination(s)
   config.log_to = %w[stdout file]
 
