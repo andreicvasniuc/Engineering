a06bd3d4f61f45cecae6576d1ee0389ea670d5f2
andreicvasniuc <andrei.a.cvasniuc@gmail.com>
2016-08-22 20:34:51 +0300

    commented unnecesary code to implement CORS

--- src/ishop/ishop.web/config/application.rb
+++ src/ishop/ishop.web/config/application.rb
@@ -45,22 +45,22 @@ module IshopWeb
         controller_specs: true
     end
 
-    config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
-      allow do
-        origins '*'
+    # config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
+    #   allow do
+    #     origins '*'
 
-        resource '/cors',
-          :headers => :any,
-          :methods => [:post],
-          :credentials => true,
-          :max_age => 0
+    #     resource '/cors',
+    #       :headers => :any,
+    #       :methods => [:post],
+    #       :credentials => true,
+    #       :max_age => 0
 
-        resource '*',
-          :headers => :any,
-          :methods => [:get, :post, :delete, :put, :patch, :options, :head],
-          :max_age => 0
-      end
-    end
+    #     resource '*',
+    #       :headers => :any,
+    #       :methods => [:get, :post, :delete, :put, :patch, :options, :head],
+    #       :max_age => 0
+    #   end
+    # end
     
   end
 end
