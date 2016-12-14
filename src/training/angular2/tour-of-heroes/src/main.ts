f3a2194dba6bca298a9baa207cbb2b4f638fc0f8
andrew <andrew-PC@andrews-MacBook-Pro.local>
2016-12-14 19:22:26 +0200

    added tour-of-heroes app

---
+++ src/training/angular2/tour-of-heroes/src/main.ts
@@ -0,0 +1,7 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
+
+import { AppModule } from './app/app.module'
+
+platformBrowserDynamic().bootstrapModule(AppModule);
+    // .then(success => console.log('Bootstrap success'))
+    // .catch(error => console.error(error));
