040dafc4558f758545e0cc9ed3c628d1342142f0
andreicvasniuc <andrei.a.cvasniuc@gmail.com>
2017-07-13 20:24:53 +0300

    ishop -> app: implemented top products

--- src/ishop/ishop.web/app/models/product.rb
+++ src/ishop/ishop.web/app/models/product.rb
@@ -7,15 +7,8 @@ class Product
   embeds_many :images, class_name: "ProductImage"
 
   def self.top_list
-    # products = Collection.only(:products).where("products.is_top" => true).pluck(:products).flatten
-    # products_json = Collection.only(:products).pluck(:products).flatten
-    # products = products_json.map do |product_json| 
-    #   product = self.new(product_json)
-    #   product.create_image_url()
-    # end
-    collections = Collection.only(:products)
-    products = collections.map { |collection| collection.products }.flatten[0..5]
-    iputs products.size
+    collections = Collection.only(:products).where("products.is_top" => true)
+    products = collections.map { |collection| collection.products }.flatten
     products
   end
 end
